87d1cd8ff93ec658242a7018b2d28e2fb16b3012
jessicajakoby <53048417+jessicajakoby@users.noreply.github.com>
2019-11-21 21:35:24 -0500

    Create MainRecommender.js

---
+++ MainRecommender.js
@@ -0,0 +1,349 @@
+import React, { Component } from 'react';
+import { Button, ImageBackground, Platform, StyleSheet, Text, View } from 'react-native';
+
+import FontAwesome, { SolidIcons, RegularIcons, BrandIcons } from 'react-native-fontawesome';
+import { parseIconFromClassName } from 'react-native-fontawesome';
+import * as Font from 'expo-font';
+import  IconButton  from './IconButton';
+import { withNavigationFocus } from 'react-navigation';
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+});
+const validIcon = parseIconFromClassName('fas fa-plus')
+
+class MainRecommender extends Component {
+  constructor(props) {
+    super(props);
+    this.user = "";
+    this.emergencyRoomButtonClick = this.emergencyRoomButtonClick.bind(this);
+    this.detoxButtonClick = this.detoxButtonClick.bind(this);
+    this.inpatientButtonClick = this.inpatientButtonClick.bind(this);
+    this.outpatientButtonClick = this.outpatientButtonClick.bind(this);
+    this.soberhouseButtonClick = this.soberhouseButtonClick.bind(this);
+    this.supportgroupButtonClick = this.supportgroupButtonClick.bind(this);
+
+    this.state = {
+      emergencyButtonClicked : false,
+      detoxButtonClicked: false,
+      inpatientrehabButtonClicked: false,
+      outpatientrehabButtonClicked: false,
+      soberhouseButtonClicked: false,
+      supportgroupButtonClicked: false};
+  }
+  state = {
+    fontLoaded: false,
+  };
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Font Awesome': require('./assets/fonts/fontawesome.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+  render() {
+    try {
+        this.user = this.props.navigation.state.params.user;
+    }
+    catch(error) {
+    }
+    return (
+      <ImageBackground
+        source={require('./images/nopioid-banner.png')}
+        style={styles.imageBackground}>
+        <View style={styles.mainContainer}>
+          <View style={styles.actionBar}>
+            {
+              this.state.fontLoaded ? (
+                <Text style={styles.actionButtons}>&#xf0c9;</Text>
+              ) : null
+            }
+            <Text style={styles.mainTitle}>Nopioid</Text>
+            {
+              this.state.fontLoaded ? (
+                <Text style={styles.actionButtons}>&#xf007;</Text>
+              ) : null
+            }
+          </View>
+          {
+            this.user != "" ? (
+              <Text style={styles.welcomeTitle}>Hey, {this.user}!</Text>
+            ): <Text style={styles.welcomeTitle}></Text>
+          }
+          <View style={styles.card}>
+            <Text style={styles.cardTitle}>What are you looking for?</Text>
+            <View style={styles.cardContent}>
+              {
+                this.state.emergencyButtonClicked ?
+                <IconButton
+                  ref={
+                    (comp)=>this.emergencyRoomButton = comp
+                  }
+                  style={styles.cardContentButtonClicked}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf0f8;"
+                  text="Emergency Room"
+                  onPress={(comp)=>this.emergencyRoomButtonClick(comp)}/>
+                :
+
+                <IconButton
+                  ref={
+                    (comp)=>this.emergencyRoomButton = comp
+                  }
+                  style={styles.cardContentButton}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf0f8;"
+                  text="Emergency Room"
+                  onPress={(comp)=>this.emergencyRoomButtonClick(comp)}/>
+              }
+              {
+                this.state.detoxButtonClicked ?
+                <IconButton
+                  ref={
+                    (comp)=>this.detoxButton = comp
+                  }
+                  style={styles.cardContentButtonClicked}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf2cc;"
+                  text="Detox"
+                  onPress={(comp)=>this.detoxButtonClick(comp)}/>
+                :
+
+                <IconButton
+                  ref={
+                    (comp)=>this.detoxButton = comp
+                  }
+                  style={styles.cardContentButton}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf2cc;"
+                  text="Detox"
+                  onPress={(comp)=>this.detoxButtonClick(comp)}/>
+              }
+              {
+                this.state.inpatientrehabButtonClicked ?
+                <IconButton
+                  ref={
+                    (comp)=>this.inpatientrehabButton = comp
+                  }
+                  style={styles.cardContentButtonClicked}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf594;"
+                  text="Inpatient Rehab"
+                  onPress={(comp)=>this.inpatientButtonClick(comp)}/>
+                :
+
+                <IconButton
+                  ref={
+                    (comp)=>this.inpatientrehabButton = comp
+                  }
+                  style={styles.cardContentButton}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf594;"
+                  text="Inpatient Rehab"
+                  onPress={(comp)=>this.inpatientButtonClick(comp)}/>
+              }
+              {
+                this.state.outpatientrehabButtonClicked ?
+                <IconButton
+                  ref={
+                    (comp)=>this.outpatientrehabButton = comp
+                  }
+                  style={styles.cardContentButtonClicked}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf0fe;"
+                  text="Outpatient Rehab"
+                  onPress={(comp)=>this.outpatientButtonClick(comp)}/>
+                :
+
+                <IconButton
+                  ref={
+                    (comp)=>this.outpatientrehabButton = comp
+                  }
+                  style={styles.cardContentButton}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf0fe;"
+                  text="Outpatient Rehab"
+                  onPress={(comp)=>this.outpatientButtonClick(comp)}/>
+              }
+              {
+                this.state.soberhouseButtonClicked ?
+                <IconButton
+                  ref={
+                    (comp)=>this.soberhouseButton = comp
+                  }
+                  style={styles.cardContentButtonClicked}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf015;"
+                  text="Sober House"
+                  onPress={(comp)=>this.soberhouseButtonClick(comp)}/>
+                :
+
+                <IconButton
+                  ref={
+                    (comp)=>this.soberhouseButton = comp
+                  }
+                  style={styles.cardContentButton}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf015;"
+                  text="Sober House"
+                  onPress={(comp)=>this.soberhouseButtonClick(comp)}/>
+              }
+              {
+                this.state.supportgroupButtonClicked ?
+                <IconButton
+                  ref={
+                    (comp)=>this.supportgroupButton = comp
+                  }
+                  style={styles.cardContentButtonClicked}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf4c4;"
+                  text="Support Group"
+                  onPress={(comp)=>this.supportgroupButtonClick(comp)}/>
+                :
+
+                <IconButton
+                  ref={
+                    (comp)=>this.supportgroupButton = comp
+                  }
+                  style={styles.cardContentButton}
+                  iconStyle=""
+                  textStyle=""
+                  icon="&#xf4c4;"
+                  text="Support Group"
+                  onPress={(comp)=>this.supportgroupButtonClick(comp)}/>
+              }
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  }
+
+  emergencyRoomButtonClick(){
+    this.setState({emergencyButtonClicked : true});
+    // this.emergencyRoomButton.props.style = styles.cardContentButtonClicked;
+  }
+
+  detoxButtonClick(){
+    this.setState({detoxButtonClicked : true});
+    // this.emergencyRoomButton.props.style = styles.cardContentButtonClicked;
+  }
+
+  inpatientButtonClick(){
+    this.setState({inpatientrehabButtonClicked : true});
+    // this.emergencyRoomButton.props.style = styles.cardContentButtonClicked;
+  }
+
+  outpatientButtonClick(){
+    this.setState({outpatientrehabButtonClicked : true});
+    // this.emergencyRoomButton.props.style = styles.cardContentButtonClicked;
+  }
+
+  soberhouseButtonClick(){
+    this.setState({soberhouseButtonClicked : true});
+    // this.emergencyRoomButton.props.style = styles.cardContentButtonClicked;
+  }
+
+  supportgroupButtonClick(){
+    this.setState({supportgroupButtonClicked : true});
+    // this.emergencyRoomButton.props.style = styles.cardContentButtonClicked;
+  }
+
+}
+
+const styles = StyleSheet.create({
+
+  welcomeTitle:{
+    textAlignVertical: "center",
+    fontSize: 30,
+    padding: 10,
+    paddingBottom: 0,
+    color: "#fff",
+    marginTop: 10
+  },
+  mainContainer:{
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    padding: 10,
+    paddingTop: 43,
+    paddingBottom: 30,
+  },
+  imageBackground:{
+    flex: 1,
+    width: '100%',
+    height: '100%'
+  },
+  actionBar: {
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  mainTitle: {
+    fontSize: 32,
+    textAlign: 'center',
+    textAlignVertical: "center",
+    alignSelf: "center",
+    fontWeight: "500",
+    color: "#fff"
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  actionButtons:{
+    fontFamily: "Font Awesome",
+    fontSize: 35,
+    textAlign: 'center',
+    textAlignVertical: "center",
+    padding: 5,
+    width: 60,
+    color: "#fff"
+  },
+  card:{
+    marginTop: 5,
+    marginBottom: 5,
+    padding: 15,
+    paddingTop: 5,
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    borderWidth: 0.5,
+    borderColor: '#d1d1d1',
+  },
+  cardTitle:{
+    textAlign: 'center',
+    textAlignVertical: "center",
+    fontSize: 25,
+    padding: 10,
+  },
+  cardContent:{
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center"
+  },
+  cardContentButton:{
+    margin: 5,
+    width: 100
+  },
+  cardContentButtonClicked:{
+    margin: 5,
+    backgroundColor: "#00ff00",
+    width: 100
+  }
+});
+
+export default withNavigationFocus(MainRecommender);
